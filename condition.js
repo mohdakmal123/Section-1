599daac7645625aebf23f1a21b2f51be77addbca
mohdakmal123 <akmal.ahmad1209@gmail.com>
2024-07-21 20:34:36 +0530

    condition.js

---
+++ condition.js
@@ -0,0 +1,15 @@
+n = 56;
+
+if(n%2 === 0){
+    a= 'nice'
+    let b='something';
+    const c = 'something else';
+console.log('even');
+}else{
+    console.log('odd')
+
+}
+
+console.log(a);
+//console.log(b);
+console.log(c);
---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('Hello JavaScripts');
+console.log('Nice')
+
+num = 72653;
+
+myname='Akmal';
+
+console.log(myname);
+
+console.log(typeof num);
+console.log(typeof myname);
+
+console.log(1 == '1')
+console.log(1 === "1")
